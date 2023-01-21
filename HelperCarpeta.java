1577a48921c982cd4e584468204e7e5f792a036c
H4cks0r6 <102683942+H4cks0r6@users.noreply.github.com>
2023-01-21 13:24:48 +0000

    Delete HelperCarpeta.java

--- HelperCarpeta.java
+++
@@ -1,7 +0,0 @@
-import java.util.Comparator;
-class HelperCarpeta implements Comparator<Carpeta> {
-    @Override
-    public int compare(Carpeta str1, Carpeta str2){
-        return str2.getNombre().compareTo(str1.getNombre());
-    }
-}
